fd130faa729bf0d5deae6e56e16a09cc15bd2ef2
David04eng <djem12032004@gmail.com>
2026-02-23 19:43:18 -0600

    Primer commit :D

---
+++ array.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class array {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> nombres = new ArrayList<>();
+
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("Carlos");
+        nombres.add("María");
+        nombres.add("Sofía");
+
+        System.out.println("Lista de nombres:");
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+
+        System.out.println("Cantidad de nombres: " + nombres.size());
+    }
+}
